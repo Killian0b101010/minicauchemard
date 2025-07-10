dd18ee48ba49578b59986bc97b10ec674d22125f
Killianb0101010 <teixeira.killian44600@gmail.com>
2025-07-10 22:13:46 +0200

    Cd mis a jour pour Yuno e t Mathieu  qui a rater son exam et qui est nul au ping pong.

--- src/builtins/cd.c
+++ src/builtins/cd.c
@@ -6,7 +6,7 @@
 /*   By: dnahon <dnahon@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/08 19:56:08 by dnahon            #+#    #+#             */
-/*   Updated: 2025/07/10 13:32:21 by dnahon           ###   ########.fr       */
+/*   Updated: 2025/07/10 22:17:18 by dnahon           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,47 @@
 
 int	cd(int ac, char **argv, t_cd *env)
 {
+	home = getenv("HOME");
+	chdir(home);
+}
+
+char	*set_env_value(t_envv *env, const char *key, const char *value)
+{
+	while (env->envp)
+	{
+	}
+}
+int	cd(int ac, char **argv, t_envv *env)
+{
+	char		buffer[BUFFER_SIZE];
+	char		*home;
+	struct stat	st;
+
+	home = getenv("HOME");
+	// Plusieurs args
 	if (ac > 2)
 		return (fprintf(stderr, "cd : too many arguments"), 1);
-	if (ac == 1)
-		env->home_path = getenv("HOME");
-	if (!env->home_path)
-		return (fprintf(stderr, "cd : HOME not set"), 1);
+	// Ancien pwd
 	env->old_path = getenv("PWD");
-	chdir(env->home_path);
-	get_cwd(env->new_path);
-	else if (ac == 2)
+	// Si sa echoue
+	if (!home)
+		return (fprintf(stderr, "cd : HOME not set"), 1);
+	// direct le home
+	if (ac == 1)
+		go_home(home);
+	// si cd ~
+	if (ac == 2 && ft_strcmp(argv[1], "~") == 0)
+		return (go_home(home), 0);
+	// execute cd + dossier et verifie que c bien un directory
+	if (ac == 2)
 	{
+		if (stat(argv[1], &st) == 0 && S_ISDIR(st.st_mode))
+		{
+			chdir(argv[1]);
+			getcwd(buffer, BUFFER_SIZE);
+			env->pwd = ft_strdup(buffer);
+			return (0);
+		}
+		return (write(stderr, "Not a directory", 16), 1);
 	}
 }
