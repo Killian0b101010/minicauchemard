73854cbe2912006f70e0902e1e02e9c5a88b5983
Killianb0101010 <teixeira.killian44600@gmail.com>
2025-07-11 14:55:53 +0200

    mise a jour norm

--- src/builtins/cd.c
+++ src/builtins/cd.c
@@ -6,7 +6,7 @@
 /*   By: kiteixei <kiteixei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/08 19:56:08 by dnahon            #+#    #+#             */
-/*   Updated: 2025/07/11 14:50:02 by kiteixei         ###   ########.fr       */
+/*   Updated: 2025/07/11 14:52:56 by kiteixei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,41 +20,42 @@ int	cd(int ac, char **argv, t_env *env)
 	chdir(home);
 }
 
-char	*set_env_value(t_env *env, const char *key, const char *value)
-{
-	char	lin
-}
-int	cd(int ac, char **argv, t_env *env)
-{
-	char		buffer[BUFFER_SIZE];
-	char		*home;
-	struct stat	st;
+// char	*set_env_value(t_env *env, const char *key, const char *value)
+// {
+// 	char	lin;
+// }
 
-	home = getenv("HOME");
-	// Plusieurs args
-	if (ac > 2)
-		return (fprintf(stderr, "cd : too many arguments"), 1);
-	// Ancien pwd
-	env->old_path = getenv("PWD");
-	// Si sa echoue
-	if (!home)
-		return (fprintf(stderr, "cd : HOME not set"), 1);
-	// direct le home
-	if (ac == 1)
-		go_home(home);
-	// si cd ~
-	if (ac == 2 && ft_strcmp(argv[1], "~") == 0)
-		return (go_home(home), 0);
-	// execute cd + dossier et verifie que c bien un directory
-	if (ac == 2)
-	{
-		if (stat(argv[1], &st) == 0 && S_ISDIR(st.st_mode))
-		{
-			chdir(argv[1]);
-			getcwd(buffer, BUFFER_SIZE);
-			env->pwd = ft_strdup(buffer);
-			return (0);
-		}
-		return (write(stderr, "Not a directory", 16), 1);
-	}
-}
+// int	cd(int ac, char **argv, t_env *env)
+// {
+// 	char		buffer[BUFFER_SIZE];
+// 	char		*home;
+// 	struct stat	st;
+
+// 	home = getenv("HOME");
+// 	// Plusieurs args
+// 	if (ac > 2)
+// 		return (fprintf(stderr, "cd : too many arguments"), 1);
+// 	// Ancien pwd
+// 	env->old_path = getenv("PWD");
+// 	// Si sa echoue
+// 	if (!home)
+// 		return (fprintf(stderr, "cd : HOME not set"), 1);
+// 	// direct le home
+// 	if (ac == 1)
+// 		go_home(home);
+// 	// si cd ~
+// 	if (ac == 2 && ft_strcmp(argv[1], "~") == 0)
+// 		return (go_home(home), 0);
+// 	// execute cd + dossier et verifie que c bien un directory
+// 	if (ac == 2)
+// 	{
+// 		if (stat(argv[1], &st) == 0 && S_ISDIR(st.st_mode))
+// 		{
+// 			chdir(argv[1]);
+// 			getcwd(buffer, BUFFER_SIZE);
+// 			env->pwd = ft_strdup(buffer);
+// 			return (0);
+// 		}
+// 		return (write(stderr, "Not a directory", 16), 1);
+// 	}
+// }
