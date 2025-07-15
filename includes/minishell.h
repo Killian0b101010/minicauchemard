e82790860e05680aed8d73f2fcdf2bf373950bea
Killianb0101010 <teixeira.killian44600@gmail.com>
2025-07-15 20:11:15 +0200

    push

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,7 +6,7 @@
 /*   By: kiteixei <kiteixei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/10 15:05:06 by dnahon            #+#    #+#             */
-/*   Updated: 2025/07/15 17:46:10 by kiteixei         ###   ########.fr       */
+/*   Updated: 2025/07/15 19:27:49 by kiteixei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -126,5 +126,5 @@ char				*get_prompt_and_input(void);
 int					process_input_line(char *input, t_env *env);
 int					is_empty_input(char *input);
 void				ft_handler(int sig);
-
+void				ft_handler2(int sig);
 #endif
